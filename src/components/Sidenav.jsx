275edc57be2953335a62566332c41ef6b1956386
Vic <vicliu325@gmail.com>
2023-02-13 23:25:58 +0800

    [add]Sidenav

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,16 @@
+//1.rafce
+import React from 'react'
+
+const Sidenav = () => {
+  return (
+    <div>Sidenav</div>
+  )
+}
+
+export default Sidenav
+
+//2.npm i react-icons --save 安裝react icons
+
+//3.Install Tailwind CSS 
+// npm install -D tailwindcss postcss autoprefixer
+// npx tailwindcss init -p
