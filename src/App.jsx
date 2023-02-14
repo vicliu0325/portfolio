81b05a5b38ca6344256720da979fdad6fbb746af
Vic <vicliu325@gmail.com>
2023-02-14 17:02:45 +0800

    [import]<Sidenav />

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Sidenav from './components/Sidenav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-
+      <Sidenav />
     </div>
   )
 }
