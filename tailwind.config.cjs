d1f2b6b90739a52d276760e5f3350058412f3ddd
Vic <vicliu325@gmail.com>
2023-02-14 16:48:39 +0800

    Configure your template paths

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Configure your template paths
+  // Add the paths to all of your template files in your tailwind.config.cjs file.
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
