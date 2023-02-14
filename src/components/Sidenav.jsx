5d10d66bc492ecd5d93ea4f433468105eab865cc
Vic <vicliu325@gmail.com>
2023-02-14 22:19:50 +0800

    [edit]md layout

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -17,11 +17,11 @@ const Sidenav = () => {
   }
   return (
     <div>
-      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <AiOutlineMenu onClick={handleNav} className='agsolute top-4 right-4 z-[99] md:hidden'/>
       { 
         //if nav state is true
         nav ? (
-          <div>
+          <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
             <a href='#main'>
               <AiOutlineHome size={20}/>
               <span className='pl-4'>Home</span>
