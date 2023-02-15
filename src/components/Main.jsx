625576eed1fdaee8a6df6b812b10bf171cb08ad4
Vic <vicliu325@gmail.com>
2023-02-15 16:48:37 +0800

    [finished]Main.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-//rfce
+//rafce
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation';
 import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
@@ -11,9 +11,9 @@ function Main() {
         {/*在背景圖片上加半透明白色背景*/}
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             {/* 安裝react typed animation 製作打字效果 */}
-            <div className=''>
-                <h1>I'm Vic Liu</h1>
-                <h2>I'm a
+            <div className='max-w-[700px] m-auto w-full h-full flex flex-col justify-center lg:items-start items-center'>
+                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Vic Liu</h1>
+                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a
                 <TypeAnimation
                     sequence={[
                         'Developer', // Types 'One'
@@ -28,7 +28,7 @@ function Main() {
                 />
                 </h2>
                 {/* 加入社群連結icon */}
-                <div className=''>
+                <div className='flex justify-between pt-6 max-w-[200px] w-full'>
                     <FaTwitter className='cursor-pointer' size={20}/>
                     <FaFacebookF className='cursor-pointer' size={20}/>
                     <FaInstagram className='cursor-pointer' size={20}/>
