ead6633d95ff84f8db1741536a19d9fc1fb56c8f
Vic <vicliu325@gmail.com>
2023-02-15 14:27:52 +0800

    [add]bg-photo

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+//rfce
+import React from 'react'
+
+function Main() {
+  return (
+    <div id='main'>
+        <img className='' src='https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80' alt=''/>
+    </div>
+  )
+}
+
+export default Main
