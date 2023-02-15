489424dc67caabd6feafc8341616e822ab8369bd
Vic <vicliu325@gmail.com>
2023-02-16 01:53:00 +0800

    [edit]sequence

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -37,10 +37,12 @@ const Sidenav = () => {
               <AiOutlineProject size={20}/>
               <span className='pl-4'>Projects</span>
             </a>
+
             <a href='#work' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <GrProjects size={20}/>
               <span className='pl-4'>Work</span>
             </a>
+  
             <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <BsPerson size={20}/>
               <span className='pl-4'>Resume</span>
@@ -61,11 +63,11 @@ const Sidenav = () => {
             <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300'>
               <AiOutlineHome scale={20}/>
             </a>
-            <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300'>
-              <GrProjects scale={20}/>
-            </a>
             <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300'>
               <AiOutlineProject scale={20}/>
+            </a>
+            <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300'>
+              <GrProjects scale={20}/>
             </a>
             <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300'>
               <BsPerson scale={20}/>
