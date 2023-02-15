5df9cadf2a20b15d4e00a5addd274c648fef1426
Vic <vicliu325@gmail.com>
2023-02-15 13:40:42 +0800

    [add]others icon

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -26,6 +26,22 @@ const Sidenav = () => {
               <AiOutlineHome size={20}/>
               <span className='pl-4'>Home</span>
             </a>
+            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineHome size={20}/>
+              <span className='pl-4'>Work</span>
+            </a>
+            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineHome size={20}/>
+              <span className='pl-4'>Projects</span>
+            </a>
+            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineHome size={20}/>
+              <span className='pl-4'>Resume</span>
+            </a>
+            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineHome size={20}/>
+              <span className='pl-4'>Contact</span>
+            </a>
           </div>
         ):(
         // else nav state is false
