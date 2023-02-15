509bdf81708e55ed1bc8a589ad7a578aabccd153
Vic <vicliu325@gmail.com>
2023-02-16 01:48:23 +0800

    [add]scroll-smooth in index.css

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -33,13 +33,13 @@ const Sidenav = () => {
               <AiOutlineHome size={20}/>
               <span className='pl-4'>Home</span>
             </a>
-            <a href='#work' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-              <GrProjects size={20}/>
-              <span className='pl-4'>Work</span>
-            </a>
             <a href='#projects' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineProject size={20}/>
               <span className='pl-4'>Projects</span>
+            </a>
+            <a href='#work' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <GrProjects size={20}/>
+              <span className='pl-4'>Work</span>
             </a>
             <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <BsPerson size={20}/>
