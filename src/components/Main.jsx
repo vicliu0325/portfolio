6fa29e580663d251e16d0f03f55b0aa7a0f3a5f1
Vic <vicliu325@gmail.com>
2023-02-15 15:15:09 +0800

    [add]bg-white opacity50 on the top of the bg-photo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 function Main() {
   return (
     <div id='main'>
-        <img className='' src='https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80' alt=''/>
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src='https://images.unsplash.com/photo-1502325966718-85a90488dc29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt=''/>
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'></div>
     </div>
   )
 }
