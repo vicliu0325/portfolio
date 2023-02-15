4aa9ec1788e1ecd3590e8a53a1a6b5042c42b3d3
Vic <vicliu325@gmail.com>
2023-02-15 13:52:30 +0800

    [edit]react-icon

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -5,7 +5,14 @@ import React, {useState} from 'react'
 //3.Install Tailwind CSS 
 // npm install -D tailwindcss postcss autoprefixer
 // npx tailwindcss init -p
-import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineMenu, 
+        AiOutlineHome, 
+        AiOutlineProject,
+        AiOutlineMail,
+} from 'react-icons/ai';
+import {BsPerson} from 'react-icons/bs';
+import {GrProjects} from 'react-icons/gr';
+
 
 
 const Sidenav = () => {
@@ -27,19 +34,19 @@ const Sidenav = () => {
               <span className='pl-4'>Home</span>
             </a>
             <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-              <AiOutlineHome size={20}/>
+              <GrProjects size={20}/>
               <span className='pl-4'>Work</span>
             </a>
             <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-              <AiOutlineHome size={20}/>
+              <AiOutlineProject size={20}/>
               <span className='pl-4'>Projects</span>
             </a>
             <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-              <AiOutlineHome size={20}/>
+              <BsPerson size={20}/>
               <span className='pl-4'>Resume</span>
             </a>
             <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-              <AiOutlineHome size={20}/>
+              <AiOutlineMail size={20}/>
               <span className='pl-4'>Contact</span>
             </a>
           </div>
