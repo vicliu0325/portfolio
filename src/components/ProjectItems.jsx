2e3a59cb414671787a7d014cccf6632043f1ba9d
Vic <vicliu325@gmail.com>
2023-02-16 01:44:07 +0800

    [finish]project

--- src/components/ProjectItems.jsx
+++ src/components/ProjectItems.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const ProjectItems = () => {
+const ProjectItems = ({img,title}) => {
   return (
-    <div>ProjectItems</div>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
+      <img src={img} alt='/' className='rounded-xl group-hover:opacity-10'/>
+      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <h3 className='text-2xl font-bold text-white tracking-wider text-center pb-4 pt-2'>
+          {title}
+        </h3>
+        {/* <p className='pb-4 pt-2 text-white text-center'>React</p> */}
+        <a href='/'>
+          <p className='text-center p-3 rounded-lg bg-white to-gray-700 font-bold cursor-pointer text-lg'>More Info</p>
+        </a>
+      </div>
+    </div>
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,10 +9,10 @@ const Projects = () => {
     <div id='projects' className='max-w-[1040px] m-auto md:pl-28 p-4 py-16'>
         <h1 className='text-4xl font-bold text-center text-[#001b5e] mb-8'>Projects</h1>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItems img={ebookImg} title='缺E-BOOK 電子書販售平台'/>
+            <ProjectItems img={ebookImg} title='缺E-BOOK電子書販售平台'/>
             <ProjectItems img={maskMapImg} title='口罩地圖'/>
-            <ProjectItems img={exploreMapImg} title='2023 台灣光影藝術節導覽地圖'/>
-            <ProjectItems />
+            <ProjectItems img={exploreMapImg} title='台灣光影藝術節導覽地圖'/>
+            {/* <ProjectItems /> */}
         </div>
     </div>
   )
