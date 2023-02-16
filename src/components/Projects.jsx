0e2d21f10a4c494df6e42b5ed95b2766897eeb98
Vic <vicliu325@gmail.com>
2023-02-16 23:39:17 +0800

    [add]Project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,10 +12,11 @@ const Projects = () => {
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
             <img src={ebookImg} alt='/' className='rounded-xl group-hover:opacity-10'/>
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                <h3 className='text-xl font-bold text-white tracking-wider text-center pb-4 pt-2'>
+                <h3 className='text-xl font-bold text-white tracking-wider text-center pb-2 pt-2'>
                 缺E-BOOK
                 <br />電子書販售平台
                 </h3>
+                <p className='pb-4 text-white text-center text-sm'>React / Next.js + Chat engine.io</p>
                 {/* <p className='pb-4 pt-2 text-white text-center'>React</p> */}
                 
                 <a href='https://github.com/Anthya1104/ebook-fe'>
@@ -27,13 +28,14 @@ const Projects = () => {
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
             <img src={maskMapImg} alt='/' className='rounded-xl group-hover:opacity-10'/>
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                <h3 className='text-xl font-bold text-white tracking-wider text-center pb-4 pt-2'>
+                <h3 className='text-xl font-bold text-white tracking-wider text-center pb-2 pt-2'>
                 口罩實名制
                 <br />即時庫存地圖
                 </h3>
                 {/* <p className='pb-4 pt-2 text-white text-center'>React</p> */}
                 
                 <a href='https://vicliu0325.github.io/mask-map/'>
+                <p className='pb-4 text-white text-center text-sm'>Leaflet + OpenStreetMap api</p>
                 <p className='text-center p-2 rounded-lg bg-white to-gray-700 font-bold cursor-pointer text-lg hover:scale-110 ease-in duration-300'>More Info
                 </p>
                 </a>
@@ -42,12 +44,12 @@ const Projects = () => {
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
             <img src={exploreMapImg} alt='/' className='rounded-xl group-hover:opacity-10'/>
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                <h3 className='text-xl font-bold text-white tracking-wider text-center pb-4 pt-2'>
+                <h3 className='text-xl font-bold text-white tracking-wider text-center pb-2 pt-2'>
                 台灣光影藝術節
                 <br />導覽地圖
                 </h3>
                 {/* <p className='pb-4 pt-2 text-white text-center'>React</p> */}
-                
+                <p className='pb-4 text-white text-center text-sm'>Google Map api</p>
                 <a href='https://taiwaninternationallightfestival.000webhostapp.com/' >
                 <p className='text-center p-2 rounded-lg bg-white to-gray-700 font-bold cursor-pointer text-lg hover:scale-110 ease-in duration-300'>More Info
                 </p>
